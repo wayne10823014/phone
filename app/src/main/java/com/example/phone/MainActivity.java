b1041f96e1da44df430bb7d0c4b768ff4adadec6
wayne10823014 <514041@stu.chsc.tw>
2021-05-20 22:50:16 +0800

    成功加入AlertDialog

--- app/src/main/java/com/example/phone/MainActivity.java
+++ app/src/main/java/com/example/phone/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.phone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -16,16 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.btn);
         button.setOnClickListener(new View.OnClickListener() { //點擊後執行跳頁的指令
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this, intent.class);
                 startActivity(intent);
+                dialog();
             }
-
         });
     }
+    private void dialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提示");  //設置標題
+        builder.setIcon(R.mipmap.ic_launcher_round); //標題前面那個小圖示
+        builder.setMessage("成功開啟對話框囉"); //提示訊息
 
+
+        //確定 取消 一般 這三種按鈕就看你怎麼發揮你想置入的功能囉
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.create().show();
+    }
 }

4ea7bf1d2c8ae4673c758449eb80ef1f3e556974
wayne10823014 <514041@stu.chsc.tw>
2021-05-20 23:11:49 +0800

    完成呼叫輪椅的訊息通知

--- app/src/main/java/com/example/phone/MainActivity.java
+++ app/src/main/java/com/example/phone/MainActivity.java
@@ -25,30 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this, intent.class);
                 startActivity(intent);
-                dialog();
             }
         });
-    }
-    private void dialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("提示");  //設置標題
-        builder.setIcon(R.mipmap.ic_launcher_round); //標題前面那個小圖示
-        builder.setMessage("成功開啟對話框囉"); //提示訊息
-
-
-        //確定 取消 一般 這三種按鈕就看你怎麼發揮你想置入的功能囉
-        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-        builder.create().show();
     }
 }
--- app/src/main/java/com/example/phone/intent.java
+++ app/src/main/java/com/example/phone/intent.java
@@ -2,13 +2,35 @@ package com.example.phone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class intent extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
+    }
+    public void callWheelchair(View v) {
+        dialog();
+    }
+    private void dialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("訊息");  //設置標題
+        builder.setIcon(R.mipmap.ic_launcher_round); //標題前面那個小圖示
+        builder.setMessage("執行中 請稍後"); //提示訊息
+
+        //確定 取消 一般 這三種按鈕就看你怎麼發揮你想置入的功能囉
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.create().show();
     }
 }
